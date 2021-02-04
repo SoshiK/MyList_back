91386f1488e3305458c2827fcb308a3aa5663f80
Soshi <SoshiK@users.noreply.github.com>
2021-02-04 15:20:43 +0900

    modified ssl

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,13 @@
 module.exports = {
   client: "pg",
   connection:
-    process.env.DATABASE_URL + "?sslmode=require&ssl=true" ||
+    process.env.DATABASE_URL ||
     `postgres://${process.env.USER}@127.0.0.1:5432/js_lists`,
   searchPath: "public",
   migrations: {
-    directory: "./migrations",
+    directory: "./migrations"
+  },
+  ssl: {
+    rejectUnauthorized: false
   }
 };
